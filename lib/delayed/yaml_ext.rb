e764c912a3d2398bd229bc444692585200fa8b3d
Soichiro Ogawa <ogawaso@gmail.com>
2013-08-14 17:13:50 +0900

    remove psych extension to work serialize method of Rails

--- lib/delayed/yaml_ext.rb
+++ lib/delayed/yaml_ext.rb
@@ -6,5 +6,5 @@ if YAML.parser.class.name =~ /syck|yecht/i
   require File.expand_path('../syck_ext', __FILE__)
   require File.expand_path('../serialization/active_record', __FILE__)
 else
-  require File.expand_path('../psych_ext', __FILE__)
+#  require File.expand_path('../psych_ext', __FILE__)
 end
